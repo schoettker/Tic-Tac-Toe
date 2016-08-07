ec5e15f8a0cd8c893c0c9b41bcdbb8addddca361
Schoettker <lennartschoettker@hotmail.com>
2016-08-07 18:05:45 +0200

    add spawnFields method to board prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // business logic
+var fields = [];
 function Player(mark, score){
   this.mark = mark;
   this.score = score;
 };
-function Space(x, y, mark){
+function Field(x, y, mark){
   this.xCoordinate = x;
   this.yCoordinate = y;
   this.mark = mark;
@@ -18,6 +19,23 @@ Board.prototype.findField = function (x, y) {
     }
   })
 };
+Board.prototype.spawnFields = function () {
+  // bottom row y = 0
+  for (var i = 0; i < 3; i++) {
+    var newField = new Field(i ,0);
+    fields.push(newField);
+    };
+  // middle row y = 1
+  for (var i = 0; i < 3; i++) {
+    var newField = new Field(i ,1);
+    fields.push(newField);
+    };
+  // top row y = 2
+  for (var i = 0; i < 3; i++) {
+    var newField = new Field(i ,2);
+    fields.push(newField);
+    };
+  };
 function Game(){
 
-}
+};
