1a20ab94ceebbc32f3fee74091a88adb1cf2abba
Schoettker <lennartschoettker@hotmail.com>
2016-08-07 18:15:42 +0200

    remove square fields from html and add them to the spawnFields method

--- js/scripts.js
+++ js/scripts.js
@@ -21,21 +21,31 @@ Board.prototype.findField = function (x, y) {
 };
 Board.prototype.spawnFields = function () {
   // bottom row y = 0
+  $(".board").append("<div class='row bottom'></div>")
   for (var i = 0; i < 3; i++) {
     var newField = new Field(i ,0);
     fields.push(newField);
+    $(".bottom").append("<div class='square'></div>")
     };
   // middle row y = 1
+  $(".board").append("<div class='row middle'></div>")
   for (var i = 0; i < 3; i++) {
     var newField = new Field(i ,1);
     fields.push(newField);
+    $(".middle").append("<div class='square'></div>")
     };
   // top row y = 2
+  $(".board").append("<div class='row top'></div>")
   for (var i = 0; i < 3; i++) {
     var newField = new Field(i ,2);
     fields.push(newField);
+    $(".top").append("<div class='square'></div>")
     };
   };
 function Game(){
 
 };
+$(document).ready(function(){
+  var board = new Board();
+  board.spawnFields();
+});
